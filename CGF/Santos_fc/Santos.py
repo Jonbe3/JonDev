9959d2d00e61cca244122c179a0b1fb5b44b40b7
JoãoSátiro <96781821+Jonbe3@users.noreply.github.com>
2026-08-12 08:58:28 -0300

    feat: Renderização do escudo do Santos com textura OpenGL

--- CGF/Santos_fc/Santos.py
+++ CGF/Santos_fc/Santos.py
@@ -1,7 +1,39 @@
 import glfw
+import ctypes
+import numpy as np
+
 from OpenGL.GL import *
 from PIL import Image
 from pathlib import Path
+
+def criar_quadrado():
+    vertices = np.array([
+        # posição       # textura
+        -0.8,  0.8,     0.0, 1.0,  # superior esquerdo
+         0.8,  0.8,     1.0, 1.0,  # superior direito
+         0.8, -0.8,     1.0, 0.0,  # inferior direito
+        -0.8, -0.8,     0.0, 0.0,  # inferior esquerdo
+    ], dtype=np.float32)
+
+    return vertices
+
+def criar_vbo(vertices):
+    vbo = glGenBuffers(1)
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        vertices.nbytes,
+        vertices,
+        GL_STATIC_DRAW
+    )
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+    return vbo
+
+
 
 def carregar_textura(caminho):
     imagem = Image.open(caminho)
@@ -38,6 +70,121 @@ def carregar_textura(caminho):
 
     return textura
 
+def criar_vao(vbo):
+    vao = glGenVertexArrays(1)
+
+    glBindVertexArray(vao)
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+
+    # Posição
+    glVertexAttribPointer(
+        0,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        4 * 4,
+        ctypes.c_void_p(0)
+    )
+
+    glEnableVertexAttribArray(0)
+
+    # Coordenadas da textura
+    glVertexAttribPointer(
+        1,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        4 * 4,
+        ctypes.c_void_p(2 * 4)
+    )
+
+    glEnableVertexAttribArray(1)
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    glBindVertexArray(0)
+
+    return vao
+
+def criar_vertex_shader():
+    codigo = """
+    #version 330 core
+
+    layout (location = 0) in vec2 posicao;
+    layout (location = 1) in vec2 coordenada_textura;
+
+    out vec2 texCoord;
+
+    void main()
+    {
+        gl_Position = vec4(posicao, 0.0, 1.0);
+        texCoord = coordenada_textura;
+    }
+    """
+
+    shader = glCreateShader(GL_VERTEX_SHADER)
+
+    glShaderSource(shader, codigo)
+    glCompileShader(shader)
+
+    if not glGetShaderiv(shader, GL_COMPILE_STATUS):
+        erro = glGetShaderInfoLog(shader).decode()
+        print("Erro no Vertex Shader:")
+        print(erro)
+
+    return shader
+
+def criar_fragment_shader():
+    codigo = """
+    #version 330 core
+
+    in vec2 texCoord;
+
+    out vec4 cor;
+
+    uniform sampler2D textura;
+
+    void main()
+    {
+        cor = texture(textura, texCoord);
+    }
+    """
+
+    shader = glCreateShader(GL_FRAGMENT_SHADER)
+
+    glShaderSource(shader, codigo)
+    glCompileShader(shader)
+
+    if not glGetShaderiv(shader, GL_COMPILE_STATUS):
+        erro = glGetShaderInfoLog(shader).decode()
+        print("Erro no Fragment Shader:")
+        print(erro)
+
+    return shader
+
+def criar_programa_shader():
+    vertex_shader = criar_vertex_shader()
+    fragment_shader = criar_fragment_shader()
+
+    programa = glCreateProgram()
+
+    glAttachShader(programa, vertex_shader)
+    glAttachShader(programa, fragment_shader)
+
+    glLinkProgram(programa)
+
+    if not glGetProgramiv(programa, GL_LINK_STATUS):
+        erro = glGetProgramInfoLog(programa).decode()
+        print("Erro ao linkar os shaders:")
+        print(erro)
+
+    glDeleteShader(vertex_shader)
+    glDeleteShader(fragment_shader)
+
+    return programa
+
+
+
 def main():
 
     # Iniciando GLFW
@@ -57,24 +204,48 @@ def main():
     glfw.make_context_current(janela)
     caminho_escudo = Path(__file__).parent / "escudo.jpg"
     textura_escudo = carregar_textura(caminho_escudo)
+    vertices = criar_quadrado()
+    vbo = criar_vbo(vertices)
+    vao = criar_vao(vbo)
+
+    shader_program = criar_programa_shader()
 
     # Loop principal
     while not glfw.window_should_close(janela):
 
-        # Cor de fundo  
+    
+
+        # Cor de fundo
         glClearColor(0.05, 0.05, 0.05, 1.0)
 
-        # Limpar tela
+        # Limpa a tela
         glClear(GL_COLOR_BUFFER_BIT)
 
-        # Atualiza janela
+        # Usa nosso shader
+        glUseProgram(shader_program)
+        local_textura = glGetUniformLocation(shader_program, "textura")
+        glUniform1i(local_textura, 0)
+
+        # Usa nosso VAO
+        glBindVertexArray(vao)
+
+        # Usa a textura do escudo
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, textura_escudo)
+        
+        
+        # Desenha os 4 vértices
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4)
+
+        # Desvincula
+        glBindVertexArray(0)
+        glUseProgram(0)
+
+        # Atualiza a janela
         glfw.swap_buffers(janela)
 
         # Processa eventos
         glfw.poll_events()
-
-    # Encerra GLFW
-    glfw.terminate()
 
 if __name__ == "__main__":
     main()
